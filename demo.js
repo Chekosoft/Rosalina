6809b7c5fa489c03ca3ea43ecf412cb027d9052d
Joaquín Muñoz M <jemunozmunoz@gmail.com>
2016-10-17 20:30:08 -0300

    demo.js has more explanations

--- demo.js
+++ demo.js
@@ -4,18 +4,23 @@ const Rosalina = require('./lib');
 //Define an instance with a remote base url.
 const app = new Rosalina('http://localhost:7654');
 
-//Define which headers will be sent to the backend.
+// Define which headers will be sent to the backend.
+// Only the defined headers will be sent, anything else is ignored.
 app.map_headers({
  'Auth-Token': function(value) {
    return {
      'Authorization': `Token=${value}`
    }
- }
+ },
+ 'X-Header-Field': 'X-Header-Backend'
 });
 
+// Defining body mappings from the local Rosalina app to the remote endpoint.
+// Only the defined fields will be sent, anything else is ignored.
 app.from('/').to('/').post({
-  a: 'a',
-  b: 'b'
+  field_name_received: 'field_name_sent',
+  rosalina_field: 'backend_field'
 });
 
+//Ready and serve.
 app.ready().listen(3000);
